5160b43f89856185e0a2a881e6e6e1c6cffae1a4
Kang <shizhouzhouya@gmail.com>
2025-10-31 18:12:55 +0800

    增加jwt安全

--- go/share/jwt/jwt.go
+++ go/share/jwt/jwt.go
@@ -1,12 +1,12 @@
 package jwt
 
 import (
-	"time"
-
 	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"time"
 )
 
-var jwtSecret = []byte("23WLW4-IOT-BIYE")
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
 type claims struct {
 	UserId int64 `json:"user_id"`
