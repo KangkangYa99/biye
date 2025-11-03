4f9b8125653c72acb8c93aa69fdda4e4482cdf39
Kang <shizhouzhouya@gmail.com>
2025-11-03 18:19:37 +0800

    修复判断密码安全的bug

--- go/share/redis/redis.go
+++ go/share/redis/redis.go
@@ -3,6 +3,8 @@ package redis
 import (
 	"context"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -11,22 +13,36 @@ var RedisClient *redis.Client
 var Nil = redis.Nil
 
 func InitRedis() {
+	addr := os.Getenv("REDIS_ADDR")
+	if addr == "" {
+		addr = "redis:6379" // 默认值
+	}
+
+	password := os.Getenv("REDIS_PASSWORD")
+	dbStr := os.Getenv("REDIS_DB")
+	db := 0
+	if dbStr != "" {
+		if d, err := strconv.Atoi(dbStr); err == nil {
+			db = d
+		}
+	}
+
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:     addr,
+		Password: password,
+		DB:       db,
 	})
 
 	ctx := context.Background()
 	_, err := RedisClient.Ping(ctx).Result()
 	if err != nil {
 		log.Printf("Redis连接失败 :%v", err)
+	} else {
+		log.Println("✅ Redis连接成功:", addr)
 	}
 }
 func CloseRedis() {
 	if RedisClient != nil {
-		if err := RedisClient.Close(); err != nil {
-			log.Printf("Redis关闭失败 %v", err)
-		}
+		RedisClient.Close()
 	}
 }
